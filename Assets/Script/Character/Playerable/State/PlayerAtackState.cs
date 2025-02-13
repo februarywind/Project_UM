e9710b43280ab4f9c71864a16c71e37bd210d610
februarywind <89524114+februarywind@users.noreply.github.com>
2025-02-13 10:29:55 +0900

    [Feat] 콤보공격 임시 구현

--- Assets/Script/Character/Playerable/State/PlayerAtackState.cs
+++ Assets/Script/Character/Playerable/State/PlayerAtackState.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class PlayerAtackState : PlayerStateBase
 {
-    public PlayerAtackState(PlayerController controller, PlayerCharacterStat characterStat, PlayerFSM playerFSM, Animator animator) : base(controller, characterStat, playerFSM, animator) {}
+    public PlayerAtackState(PlayerController controller, PlayerCharacterStat characterStat, PlayerFSM playerFSM, Animator animator, EPlayerState[] convertibleStates) : base(controller, characterStat, playerFSM, animator, convertibleStates){}
 
+    private int combo = 0;
     public override void OnStateEnter()
     {
+        controller.ConvertibleStates = convertibleStates;
         animator.SetTrigger("Atack");
     }
 
     public override void OnStateExit()
     {
+        combo = 0;
     }
 
     public override void OnStateUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetInteger("Combo", ++combo);
+        }
     }
 }
